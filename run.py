74dfb1d010f48a57aa220d9f8870d916b919c3d3
joeyh <joey@eproject-inc.com>
2014-02-20 21:58:38 -0800

    first commit

---
+++ run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import csv
+from genderPredictor import genderPredictor
+
+gp = genderPredictor()
+gp.trainAndTest()
+
+results = {
+    'numAnalyzed' : 0,
+    'numMaleAnalyzed': 0,
+    'numFemaleAnalyzed': 0,
+    'numCorrect' : 0,
+    'numMaleCorrect' : 0,
+    'numFemaleCorrect' : 0,
+    'percentageCorrect': {
+        'M': 0,
+        'F': 0,
+        'total': 0
+    }
+}
+
+with open('names.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        results['numAnalyzed'] += 1;
+        
+        gender = gp.classify(row[0])
+        
+        if gender == 'M':
+            results['numMaleAnalyzed'] += 1;
+        elif gender == 'F':
+            results['numFemaleAnalyzed'] += 1;
+        
+        if gender == row[1]:
+            results['numCorrect'] += 1
+            if gender == 'M':
+                results['numMaleCorrect'] += 1;
+            elif gender == 'F':
+                results['numFemaleCorrect'] += 1;
+
+
+results['percentageCorrect'] = {
+    'M': float(results['numMaleCorrect']) / float(results['numMaleAnalyzed']),
+    'F': float(results['numFemaleCorrect']) / float(results['numFemaleAnalyzed']),
+    'total': float(results['numCorrect']) / float(results['numAnalyzed'])
+}
+
+print results
+out = open( 'gender_predictions.json', 'w')
+out.write(str(results))
